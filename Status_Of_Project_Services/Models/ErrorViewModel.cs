20579a1f35f4827112a8586aa2d32d878f11cb41
OscarFohlin <56166104+OscarFohlin@users.noreply.github.com>
2024-03-08 10:11:48 +0100

    Init github for project
    Skapelse av projekt för övervakning

---
+++ Status_Of_Project_Services/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Status_Of_Project_Services.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Status_Of_Project_Services/wwwroot/js/site.js
@@ -0,0 +1,69 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const services = [
+        { name: "ADVERTISEMENT API", url: "https://jsonplaceholder.typicode.com/posts" },
+        { name: "ORGANISATIONS API", url: "https://informatik7.ei.hv.se/ProfilAPI/api/Organisation" },
+        { name: "PROFILE API", url: "https://informatik7.ei.hv.se/ProfilAPI/api/Profile" },
+    ];
+
+    function checkServiceStatus(url, statusElement) {
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    statusElement.textContent = "Operational";
+                } else {
+                    statusElement.textContent = "Down";
+                }
+            })
+            .catch(error => {
+                statusElement.textContent = "Down";
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
+    function checkServerStatus(url, statusElement, latencyElement) {
+        fetch(url, { method: 'HEAD' })
+            .then(response => {
+
+                if (response.ok) {
+                    statusElement.textContent = "Server Up";
+
+                } else {
+                    statusElement.textContent = "Server Down";
+                }
+            })
+            .catch(error => {
+                statusElement.textContent = "Server Down";
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
+    function checkServiceLatency(url, latencyElement) {
+        const startTime = performance.now();
+        fetch(url)
+            .then(response => {
+                const endTime = performance.now();
+                const latency = endTime - startTime;
+                latencyElement.textContent = `${latency.toFixed(2)} ms`;
+            })
+            .catch(error => {
+                latencyElement.textContent = "N/A";
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
+
+
+    services.forEach((service, index) => {
+        const serviceStatusElement = document.getElementById('serviceStatus' + (index + 1));
+        const serverStatusElement = document.getElementById('serverStatus' + (index + 1));
+        const latencyElement = document.getElementById('serviceLatency' + (index + 1));
+        checkServiceStatus(service.url, serviceStatusElement);
+        checkServerStatus(service.url, serverStatusElement);
+        checkServiceLatency(service.url, latencyElement);
+    });
+});
